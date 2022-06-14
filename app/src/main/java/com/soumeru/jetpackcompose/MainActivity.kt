f787141fb4104dd725c886b080e9bda0bf2cdba8
Mehareesh Meruga <mmeruga@we-do-it.com>
2022-06-14 15:43:01 +0530

    Rows, Columns and basic sizing of text view

--- app/src/main/java/com/soumeru/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/soumeru/jetpackcompose/MainActivity.kt
@@ -3,26 +3,38 @@ package com.soumeru.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting("Jetpack Compose")
+            /*Column(
+                modifier = Modifier
+                    .fillMaxSize(0.5f)
+                    .background(color = Color.Magenta),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text("Jetpack Compose")
+                Text("Welcome Jetpack")
+            }*/
+
+            Row(
+                modifier = Modifier
+                    .fillMaxSize(0.5f)
+                    .background(color = Color.Cyan),
+                horizontalArrangement = Arrangement.SpaceAround,
+                verticalAlignment = Alignment.CenterVertically,
+
+            ) {
+                Text("Hello")
+                Text("Jetpack")
+            }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Greeting("Jetpack Compose")
 }
