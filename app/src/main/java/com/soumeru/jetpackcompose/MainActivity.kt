290a7e18e9f85cc125931561904ab220594087f2
Mehareesh Meruga <mmeruga@we-do-it.com>
2022-06-14 16:10:22 +0530

    Modifiers and click events in Compose

--- app/src/main/java/com/soumeru/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/soumeru/jetpackcompose/MainActivity.kt
@@ -1,30 +1,38 @@
 package com.soumeru.jetpackcompose
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            /*Column(
+            Column(
                 modifier = Modifier
                     .fillMaxSize(0.5f)
-                    .background(color = Color.Magenta),
+                    .background(color = Color.Magenta)
+                    .fillMaxHeight(0.5f)
+                    .padding(50.dp),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text("Jetpack Compose")
-                Text("Welcome Jetpack")
-            }*/
+                Text("Hello", modifier = Modifier.clickable {
+                    Toast.makeText(applicationContext, "Clicked!", Toast.LENGTH_SHORT).show()
+                })
+                Spacer(modifier = Modifier.height(10.dp))
+                Text("Jetpack", modifier = Modifier.padding(start = 30.dp, top = 10.dp))
+            }
 
-            Row(
+            /*Row(
                 modifier = Modifier
                     .fillMaxSize(0.5f)
                     .background(color = Color.Cyan),
@@ -34,7 +42,7 @@ class MainActivity : ComponentActivity() {
             ) {
                 Text("Hello")
                 Text("Jetpack")
-            }
+            }*/
         }
     }
 }
