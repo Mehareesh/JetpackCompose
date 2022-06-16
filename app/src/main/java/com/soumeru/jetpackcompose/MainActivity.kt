47416e20af37860ed152450a2c02f2b8478c5123
Mehareesh Meruga <mmeruga@we-do-it.com>
2022-06-16 14:55:34 +0530

    TextView styling and handling font family in Compose

--- app/src/main/java/com/soumeru/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/soumeru/jetpackcompose/MainActivity.kt
@@ -17,11 +17,17 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -32,20 +38,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            val painter = painterResource(id = R.drawable.jetpack_compose)
-            val description = "Jetpack Compose"
-            val title = "Jetpack Compose"
-            Box(
-                modifier = Modifier
-                    .fillMaxSize(0.5f)
-                    .padding(16.dp)
-            ) {
-                ImageView(
-                    painter,
-                    description,
-                    title
-                )
-            }
+            TextViewWithFont()
         }
     }
 }
@@ -88,61 +81,100 @@ fun RowView() {
 }
 
 @Composable
-fun ImageView(
-    painter: Painter,
-    contentDescription: String,
-    title: String,
-    modifier: Modifier = Modifier
-) {
-    Card(
-        modifier = modifier.fillMaxWidth(),
-        shape = RoundedCornerShape(15.dp),
-        elevation = 5.dp
-    ) {
+fun ImageView(modifier: Modifier = Modifier) {
 
-        Box(modifier = Modifier.height(200.dp)) {
-            Image(
-                painter = painter,
-                contentDescription = contentDescription,
-                contentScale = ContentScale.Crop
-            )
-            
-            Box(modifier = Modifier
-                .fillMaxSize()
-                .background(
-                    Brush.verticalGradient(
-                        colors = listOf(
-                            Color.Transparent,
-                            Color.Black
-                        ), startY = 300f
-                    )
-                )
-            )
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(12.dp),
-                contentAlignment = Alignment.BottomStart
-            ) {
-                Text(text = title, style = TextStyle(color = Color.White), fontSize = 16.sp)
-            }
-        }
-
-    }
-
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultView() {
     val painter = painterResource(id = R.drawable.jetpack_compose)
-    val description = "Jetpack Compose"
+    val contentDescription = "Jetpack Compose"
     val title = "Jetpack Compose"
+
     Box(
         modifier = Modifier
             .fillMaxSize(0.5f)
             .padding(16.dp)
     ) {
-        ImageView(painter = painter, contentDescription = description, title = title)
+        Card(
+            modifier = modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(15.dp),
+            elevation = 5.dp
+        ) {
+
+            Box(modifier = Modifier.height(200.dp)) {
+                Image(
+                    painter = painter,
+                    contentDescription = contentDescription,
+                    contentScale = ContentScale.Crop
+                )
+
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(
+                            Brush.verticalGradient(
+                                colors = listOf(
+                                    Color.Transparent,
+                                    Color.Black
+                                ), startY = 300f
+                            )
+                        )
+                )
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(12.dp),
+                    contentAlignment = Alignment.BottomStart
+                ) {
+                    Text(text = title, style = TextStyle(color = Color.White), fontSize = 16.sp)
+                }
+            }
+
+        }
     }
+}
+
+@Composable
+fun TextViewWithFont() {
+
+    val fontFamily = FontFamily(
+        Font(R.font.roboto_condensed_light, FontWeight.Light),
+        Font(R.font.roboto_condensed_bold, FontWeight.Bold),
+        Font(R.font.roboto_condensed_bold_italic, FontWeight.ExtraBold),
+        Font(R.font.roboto_condensed_italic, FontWeight.SemiBold),
+        Font(R.font.roboto_condensed_light_italic, FontWeight.ExtraLight),
+        Font(R.font.roboto_condensed_regular, FontWeight.Normal)
+    )
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFF05885E))
+    ) {
+        Text(
+            text = buildAnnotatedString {
+                withStyle(
+                    style = SpanStyle(color = Color.Red, fontSize = 50.sp)
+                ) {
+                    append("J")
+                }
+                append("etpack ")
+                withStyle(
+                    style = SpanStyle(color = Color.Red, fontSize = 50.sp)
+                ) {
+                    append("C")
+                }
+                append("ompose")
+            },
+            color = Color.White,
+            fontSize = 30.sp,
+            fontFamily = fontFamily,
+            fontWeight = FontWeight.Bold,
+            fontStyle = FontStyle.Italic,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultView() {
+    TextViewWithFont()
 }
