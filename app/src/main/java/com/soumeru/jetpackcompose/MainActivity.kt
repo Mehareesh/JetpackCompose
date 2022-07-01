2795cb835cd44ac34016b0b8715d850ea318643b
Mehareesh Meruga <mehareeshm@gmail.com>
2022-07-01 15:21:34 +0530

    Animated Circular Progress Bar in Jetpack Compose

--- app/src/main/java/com/soumeru/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/soumeru/jetpackcompose/MainActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -19,6 +21,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.res.painterResource
@@ -32,6 +36,8 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ChainStyle
@@ -47,7 +53,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            ConstraintLayoutView()
+            CircularAnimatedProgressBar()
         }
     }
 }
@@ -328,8 +334,64 @@ fun ConstraintLayoutView() {
     }
 }
 
+@Composable
+fun CircularAnimatedProgressBar() {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        val percentage = 0.75f
+        val number = 100
+        val fontSize: TextUnit = 25.sp
+        val radius: Dp = 50.dp
+        val color: Color = Color.Magenta
+        val strokeWidth: Dp = 8.dp
+        val animDuration = 1000
+        val animDelay = 0
+
+        var animationPlayed by remember {
+            mutableStateOf(false)
+        }
+
+        val currentPercentage = animateFloatAsState(
+            targetValue = if (animationPlayed) percentage else 0f,
+            animationSpec = tween(
+                durationMillis = animDuration,
+                delayMillis = animDelay
+            )
+        )
+        LaunchedEffect(key1 = true) {
+            animationPlayed = true
+        }
+
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier.size(radius * 2f)
+        ) {
+            Canvas(modifier = Modifier.size(radius * 2f)) {
+                drawArc(
+                    color = color,
+                    -90f,
+                    360 * currentPercentage.value,
+                    useCenter = false,
+                    style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Round)
+
+                )
+            }
+
+            Text(
+                text = (currentPercentage.value * number).toInt().toString() + "%",
+                color = Color.Black,
+                fontSize = fontSize,
+                fontWeight = FontWeight.SemiBold
+            )
+
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultView() {
-    ConstraintLayoutView()
+    CircularAnimatedProgressBar()
 }
